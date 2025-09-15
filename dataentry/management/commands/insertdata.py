29e8c6859f1a0bed25ea3b8dafdd50258c3d4232
TimzonaTimothy <timzonatimothy@gmail.com>
2025-09-15 08:14:38 -0700

    data entry app

--- dataentry/management/commands/insertdata.py
+++ dataentry/management/commands/insertdata.py
@@ -10,10 +10,15 @@ class Command(BaseCommand):
             {'matric_no': 1002, 'name':"Mike", 'age':19},
             {'matric_no': 1003, 'name':"John", 'age':18},
             {'matric_no': 1004, 'name':"Ike", 'age':20},
+            {'matric_no': 1005, 'name':"Joe", 'age':17},
         ]
 
         for data in dataset:
-          
-            students = Student.objects.create(matric_no=data['matric_no'], name=data['name'], age=data['age'])
+            matric_no=data['matric_no']
+            existing_record = Student.objects.filter(matric_no=data['matric_no']).exists()
 
-            self.stdout.write(self.style.SUCCESS(f"Data Matric No:{students.matric_no} name:{students.name} age:{students.age} inserted successfully"))
+            if not existing_record:
+                students = Student.objects.create(matric_no=data['matric_no'], name=data['name'], age=data['age'])
+                self.stdout.write(self.style.SUCCESS(f"Data Matric No:{students.matric_no} name:{students.name} age:{students.age} inserted successfully"))
+            else:
+                self.stdout.write(self.style.WARNING(f"Student with matric no {matric_no} already exits"))

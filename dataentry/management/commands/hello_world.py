49e0542c1a957efaf5a4b089da4ae8d45083a23e
TimzonaTimothy <timzonatimothy@gmail.com>
2025-09-15 08:09:01 -0700

    insert data command

--- dataentry/management/commands/hello_world.py
+++ dataentry/management/commands/hello_world.py
@@ -2,3 +2,11 @@ from django.core.management.base import BaseCommand
 
 class Command(BaseCommand):
     help = "Prints Hello world"
+
+
+    def add_arguments(self, parser):
+        parser.add_argument('name', type=str, help='Specifies name')
+
+    def handle(self, *args, **kwargs):
+        name = kwargs['name'] 
+        self.stdout.write(self.style.SUCCESS(f"Hello {name}"))
---
+++ dataentry/management/commands/insertdata.py
@@ -0,0 +1,19 @@
+from django.core.management.base import BaseCommand
+from dataentry.models import Student
+
+class Command(BaseCommand):
+    help = "Populate data to the database"
+
+    def handle(self, *args, **kwargs):
+        dataset = [
+            {'matric_no': 1001, 'name':"Ann", 'age':21},
+            {'matric_no': 1002, 'name':"Mike", 'age':19},
+            {'matric_no': 1003, 'name':"John", 'age':18},
+            {'matric_no': 1004, 'name':"Ike", 'age':20},
+        ]
+
+        for data in dataset:
+          
+            students = Student.objects.create(matric_no=data['matric_no'], name=data['name'], age=data['age'])
+
+            self.stdout.write(self.style.SUCCESS(f"Data Matric No:{students.matric_no} name:{students.name} age:{students.age} inserted successfully"))

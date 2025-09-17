391e44c5fb681bc2eb35685f11c01d0c67c36873
TimzonaTimothy <timzonatimothy@gmail.com>
2025-09-17 01:03:46 -0700

    templates and uploads

---
+++ dataentry/utils.py
@@ -0,0 +1,12 @@
+from django.apps import apps
+
+def get_all_custom_models():
+    default_models = ['Session','ContentType','Group','Permission','LogEntry','User']
+    custom_models = []
+    for model in apps.get_models():
+        if model.__name__ not in default_models:
+            custom_models.append(model.__name__)
+
+    return custom_models
+
+
--- mail_automation/settings.py
+++ mail_automation/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'dataentry',
+    'uploads',
 ]
 
 MIDDLEWARE = [
@@ -119,7 +120,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+
 STATIC_URL = '/static/'
+STATIC_ROOT = 'staticfiles/'
+STATICFILES_DIRS = [
+    BASE_DIR / 'mail_automation/static'
+]
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
